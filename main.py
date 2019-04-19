71cb2abb127053554462a8c84dfb1199667b2465
yanqiangmiffy <1185918903@qq.com>
2019-04-19 10:59:09 +0800

    update

--- main.py
+++ main.py
@@ -10,8 +10,9 @@
 import pandas as pd
 train_stock=pd.read_csv('input/TRAINSET_STOCK.csv')
 print(train_stock.info())
-print(train_stock.name.value_counts(),len(train_stock.name.value_counts()))
+print(train_stock.ts_code.value_counts(),len(train_stock.ts_code.unique()))
+# print(train_stock.name.value_counts(),len(train_stock.name.value_counts()))
 
-dates=[20190402,20190402,20190403,20190404,20190405]
-
-print(train_stock.shift(7))
+# dates=[20190402,20190402,20190403,20190404,20190405]
+#
+# print(train_stock.shift(7))
