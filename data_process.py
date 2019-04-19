73cbc132b7cd173e61068f6f2cdbc73d840f3cb8
yanqiangmiffy <1185918903@qq.com>
2019-04-19 22:50:38 +0800

    0.62566667

--- data_process.py
+++ data_process.py
@@ -10,16 +10,19 @@
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
+from sklearn.preprocessing import LabelEncoder
+lb=LabelEncoder()
 train_stock = pd.read_csv('input/TRAINSET_STOCK.csv')
 # train_stock=pd.get_dummies(train_stock,columns=['name'])
+train_stock['name']=lb.fit_transform(train_stock.name.values)
 # 设置
-no_features = ['ts_code', 'trade_date', 'y','name']
 train_stock['ts_code']=train_stock['ts_code'].astype('int32')
 train_stock['trade_date']=train_stock['trade_date'].astype('int32')
 train_stock['y']=train_stock['y'].astype('int32')
 
+no_features = ['ts_code', 'trade_date']
 features = [fea for fea in train_stock.columns if fea not in no_features]  # 11
-period = 20
+period = 40
 featurenum = len(features) * period
 future_date = [20190402, 20190403, 20190404, 20190408, 20190409]
 all_train,all_test=pd.DataFrame(),pd.DataFrame()
@@ -40,7 +43,7 @@ for index,group in tqdm(train_stock.groupby(by='ts_code')):
     df = pd.concat([group[['ts_code', 'trade_date', 'y']], df], axis=1)
 
     train, test = df.iloc[:df.shape[0] - 5], df.iloc[-5:]
-    train=train.dropna(subset=['open(day-20)','low(day-20)'],how="all")
+    train=train.dropna(subset=['pb(day-1)','pe(day-1)'],how="all")
     all_train=all_train.append(train)
     all_test=all_test.append(test)
 
